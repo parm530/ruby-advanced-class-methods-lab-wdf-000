21a53b4a2970567dd42767879527dd6cbda34092
parm530 <parmsahadeo530@gmail.com>
2016-07-20 15:34:27 -0400

    Created class constructors and implemented them in Song class

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +9,62 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new     #initiate an instance
+    self.all << song    #add the instance to the @@all class var
+    song                #returns the song instance 
+  end
+
+  def self.new_by_name(title)
+    song = self.new
+    song.name = title
+    song
+  end
+
+  def self.create_by_name(title)
+    song = self.create
+    song.name = title
+    song
+  end
+
+  def self.find_by_name(title)
+    self.all.detect{|song_name| song_name.name == title}
+  end
+
+  def self.find_or_create_by_name(title)
+    if(self.all.size == 0 || (self.name != title) )
+      self.create_by_name(title)
+    else
+      self.find_by_name(title)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|object| object.name}
+  end
+
+  def self.new_from_filename(file_name)
+    song_info = file_name.split(" - ")
+    artist = song_info[0]
+    name_song_mp3 = song_info[1]
+    name_song = name_song_mp3.split(".mp3").join
+
+
+    new_song = self.new
+    new_song.name = name_song
+    new_song.artist_name = artist
+    new_song
+  end
+
+  def self.create_from_filename(file_name)
+    file = self.new_from_filename(file_name)
+    self.all << file
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
